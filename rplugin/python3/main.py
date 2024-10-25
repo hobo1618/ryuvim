f80b9a81526299e7092f743f803acbf5e2339034
hobo1618 <hobdenw@gmail.com>
2024-10-25 19:54:00 +0000

    add open ai test connection

--- rplugin/python3/main.py
+++ rplugin/python3/main.py
@@ -1,7 +1,14 @@
 import pynvim
+from openai import OpenAI
 
 
-print("Back end is running from /backend/main.py")
+api_key = os.getenv("OPENAI_API_KEY")
+
+if not api_key:
+    raise ValueError("Please set the OPENAI_API_KEY environment variable.")
+
+# Set up the API request to OpenAI
+client = OpenAI(api_key=api_key)
 
 
 @pynvim.plugin
@@ -14,3 +21,22 @@ class Ryuvim(object):
         self.nvim.command('echo "Hello World!"')
         print("Hello World!")
         print(args)
+
+    @pynvim.command("RyuvimOpenAI", nargs="*")
+    def ryuvimOpenAI(self, args):
+        response = client.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {
+                    "role": "user",
+                    "content": "Give me a good recipe for homemade pasta.",
+                },
+            ],
+        )
+        content = response.choices[0].message.content
+        if not content:
+            raise ValueError("No content returned from OpenAI.")
+
+        self.nvim.command('echo "{}"'.format(content))
+        print("Hello Worldsss!")
