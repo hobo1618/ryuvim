749a58a6ba51a603d2ac1b9904ef15819a8d7a43
hobo1618 <hobdenw@gmail.com>
2024-10-25 12:29:18 +0000

    change command names

--- lua/ryuvim/init.lua
+++ lua/ryuvim/init.lua
@@ -10,9 +10,9 @@ M.status = marp.status
 M.toggle = marp.toggle
 
 -- create MarpStart command
-vim.api.nvim_create_user_command("MarpStart", M.start, { desc = "Start Marp" })
-vim.api.nvim_create_user_command("MarpStop", M.stop, { desc = "Stop Marp" })
-vim.api.nvim_create_user_command("MarpStatus", M.status, { desc = "Show Marp status" })
-vim.api.nvim_create_user_command("MarpToggle", M.toggle, { desc = "Toggle Marp (start/stop)" })
+vim.api.nvim_create_user_command("RyuStart", M.start, { desc = "Start Marp" })
+vim.api.nvim_create_user_command("RyuStop", M.stop, { desc = "Stop Marp" })
+vim.api.nvim_create_user_command("RyuStatus", M.status, { desc = "Show Marp status" })
+vim.api.nvim_create_user_command("RyuToggle", M.toggle, { desc = "Toggle Marp (start/stop)" })
 
 return M
