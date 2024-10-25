44e65606b65861487da199ff60bad4aa64dc24f0
hobo1618 <hobdenw@gmail.com>
2024-10-25 20:15:46 +0000

    add openai again

--- rplugin/python3/ryuvim.py
+++ rplugin/python3/ryuvim.py
@@ -1,5 +1,15 @@
 import pynvim
 from pathlib import Path
+from openai import OpenAI
+import os
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+if not api_key:
+    raise ValueError("Please set the OPENAI_API_KEY environment variable.")
+
+# Set up the API request to OpenAI
+client = OpenAI(api_key=api_key)
 
 
 @pynvim.plugin
@@ -9,27 +19,27 @@ class Ryuvim(object):
         self.cfg = nvim.exec_lua('return require("ryuvim").getConfig()')
         self.path = Path(self.cfg["path"]).expanduser()
 
-    @pynvim.function("Ryuvim")
+    @pynvim.command("Ryuvim", nargs="*")
     def ryuvim(self, args):
         self.nvim.command('echo "Hello World!"')
         print("Hello World!")
         print(args)
 
-    # @pynvim.command("RyuvimOpenAI", nargs="*")
-    # def ryuvimOpenAI(self, args):
-    #     response = client.chat.completions.create(
-    #         model="gpt-4o",
-    #         messages=[
-    #             {"role": "system", "content": "You are a helpful assistant."},
-    #             {
-    #                 "role": "user",
-    #                 "content": "Give me a good recipe for homemade pasta.",
-    #             },
-    #         ],
-    #     )
-    #     content = response.choices[0].message.content
-    #     if not content:
-    #         raise ValueError("No content returned from OpenAI.")
-    #
-    #     self.nvim.command('echo "{}"'.format(content))
-    #     print("Hello Worldsss!")
+    @pynvim.command("RyuvimOpenAI", nargs="*")
+    def ryuvimOpenAI(self, args):
+        response = client.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {
+                    "role": "user",
+                    "content": "Give me a good recipe for homemade pasta.",
+                },
+            ],
+        )
+        content = response.choices[0].message.content
+        if not content:
+            raise ValueError("No content returned from OpenAI.")
+
+        self.nvim.command('echo "{}"'.format(content))
+        print("Hello Worldsss!")
